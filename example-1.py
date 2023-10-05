fbca8bd7d03631265159c11e17ee16e9c0699659
TheOneNyangweso <morurisammy5@gmail.com>
2023-10-05 22:22:22 +0300

    "using uvicorn programmatically"

--- example-1.py
+++ example-1.py
@@ -7,8 +7,8 @@ app = FastAPI()
 
 @app.get("/")
 async def index():
-    return {"message": "Hi There"}
+    return {"message": "Hi Sam"}
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("example-1:app", host="127.0.0.2", port=8000, reload=True)
