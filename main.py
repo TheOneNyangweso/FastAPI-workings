ae0e9aa48d0efe5bbb26b7384e3d504032efb14b
TheOneNyangweso <morurisammy5@gmail.com>
2023-10-10 14:32:25 +0300

    "added validation mechanisms"

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
@@ -8,10 +8,14 @@ async def index():
     return {"message": "Hi There"}
 
 
-# Using variable param in path i.e path parameter
+# Using variable param in path i.e path parameter while applying parameter validation
 # and then passing it to path operation func as a formal param
 @app.get("/hello/{name}/{age}")
-async def hello(name: str, age: int):
+async def hello(
+    *,
+    name: str = Path(..., min_length=3, max_length=10),
+    age: int = Path(..., ge=1, le=100)
+):
     return {"Hello": name, "age": age}
 
 
@@ -21,7 +25,12 @@ async def hello(name: str, age: int):
     return {"Hello": name, "age": age}
 
 
-# COmbining path and query parameter now...
+# Combining path and query parameter now, while applying query and also path parameters validation
 @app.get("/hello/{name}")
-async def hello(name: str, age: int):
-    return {"Hello": name, "age": age}
+async def hello(
+    *,
+    age: int,
+    name: str = Path(..., min_length=3, max_length=10),
+    percent: float = Query(..., ge=0, le=100)
+):
+    return {"Hello": name, "age": age, "percentage": percent}
