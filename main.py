3721449b26340b0d81fb7ecb2cd7b2b88182ee85
TheOneNyangweso <morurisammy5@gmail.com>
2023-10-10 13:52:03 +0300

    "added endpoints to main.py"

--- main.py
+++ main.py
@@ -6,3 +6,22 @@ app = FastAPI()
 @app.get("/")
 async def index():
     return {"message": "Hi There"}
+
+
+# Using variable param in path i.e path parameter
+# and then passing it to path operation func as a formal param
+@app.get("/hello/{name}/{age}")
+async def hello(name: str, age: int):
+    return {"Hello": name, "age": age}
+
+
+# Using query parameter now...
+@app.get("/hello")
+async def hello(name: str, age: int):
+    return {"Hello": name, "age": age}
+
+
+# COmbining path and query parameter now...
+@app.get("/hello/{name}")
+async def hello(name: str, age: int):
+    return {"Hello": name, "age": age}
